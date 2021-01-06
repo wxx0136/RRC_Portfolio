c1a45ae9de1be661c1ecf8548f9a6e75b28eec73
wxx <wxx0136@gmail.com>
2021-01-05 18:54:07 -0600

    Update content.

--- src/mock/data.js
+++ src/mock/data.js
@@ -12,18 +12,18 @@ export const heroData = {
   title: 'Hi, my name is',
   name: 'Xinxin Wei',
   subtitle: "I'm a Software Engineer.",
-  cta: 'Know more',
+  cta: 'About me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'photo.jpg',
   paragraphOne:
-    'Hi, I am looking for an opportunity as a “Software Developer”. Currently, I am in my final term of Red River College in the Business Information Technology program and will graduate in December 2020.',
+    'Hi, I am looking for an opportunity as a “Software Engineer”. I just graduated from the Business Information Technology program of Red River College(RRC) in December 2020.',
   paragraphTwo:
-    "Before coming to Canada, I received my Bachelor's degree in Computer Science in China. I worked on Mobile Application (iOS) and Visual Reality (C# on Unity) Software development for several years in two firms. So, I accumulated enough theoretical knowledge and methodology to face the new challenge of technologies.",
+    "Before coming to Canada, I received my Bachelor's degree in Computer Science in China. I worked on Mobile Application (iOS) and Visual Reality (C# on Unity) Software development for several years in two firms.",
   paragraphThree:
-    'During my two years at Red River College, I worked with Dr Jon Ziprick and Dr Randal Peters on two different projects as a Student Researcher Assistant. For one project, I used Python to build a Multi-angle camera system on Raspberry Pi. And for the other, I created a 3D Model Single Web Application with React. ',
+    'While living in Winnipeg, I worked on a "Multi-angle Camera System" project as a Researcher Assistant of RRC. And I had a part-time job at ioAirFlow Inc. on a Django based web development project in 2019. From January 2020, I began a Co-op placement at Priceline.com. I used Go, Php, React.js, Git, and Docker to achieve the duties\' requirements.',
   resume: 'https://www.linkedin.com/in/xinxin-wei/', // if no resume, the button will not show up
 };
 
@@ -33,8 +33,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'project_android.png',
     title: 'Android App: Weather Forecast',
-    info:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.',
+    info: 'Lorem ipsum dolor sit, /n',
     info2: '',
     url: 'https://youtu.be/7ocbZrPZ_3I',
     repo: 'https://github.com/wxx0136/XWeather_V2', // if no repo, the button will not show up
